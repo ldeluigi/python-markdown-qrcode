8baccf0e2768bfe36639a3dfa8482bc3e1636976
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2021-05-29 15:26:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 setup(
   name='markdown_qrcode',
+  packages = find_packages(),
   version='0.0.4',
-  maintainer="none",
-  # maintainer_email="lucadelu97@gmail.com",
+  author="Many",
+  author_email="lucadelu97@gmail.com",
   url="github.com/ldeluigi/python-markdown-qrcode",
   license='LICENSE.md',
   description='A markdown extension to insert qrcode datauri images based on supplied data.',
